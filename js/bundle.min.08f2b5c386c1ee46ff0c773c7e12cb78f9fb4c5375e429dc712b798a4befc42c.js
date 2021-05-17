dbe7a5bd92d4de98f9447b2bab665200998258b5
erak <erik@zirkular.io>
2021-05-17 13:21:50 +0200

    RC 5.

---
+++ js/bundle.min.08f2b5c386c1ee46ff0c773c7e12cb78f9fb4c5375e429dc712b798a4befc42c.js
@@ -0,0 +1,21 @@
+const toggleToc=()=>{document.getElementById('toc').classList.toggle('show-toc');}
+function resizeOverlay(){var height=$('#spotlight').height();$('#overlay').height(height+1000);}
+function mapValueRange(value,inMin,inMax,outMin,outMax){return(value-inMin)*(outMax-outMin)/(inMax-inMin)+outMin;}
+var mouseX=0;var mouseY=0;var mouseThreshold=1;var scrollThreshold=1;var scrollCounter=0;var currentScroll=0;var overlays=[{src:'overlay/01.png',loaded:false},{src:'overlay/02.png',loaded:false},{src:'overlay/03.png',loaded:false},{src:'overlay/04.png',loaded:false},{src:'overlay/05.png',loaded:false},{src:'overlay/06.png',loaded:false},{src:'overlay/07.png',loaded:false},{src:'overlay/08.png',loaded:false},{src:'overlay/09.png',loaded:false},{src:'overlay/10.png',loaded:false},{src:'overlay/12.png',loaded:false},{src:'overlay/13.png',loaded:false},{src:'overlay/14.png',loaded:false},{src:'overlay/15.png',loaded:false},{src:'overlay/16.png',loaded:false},{src:'overlay/17.png',loaded:false},{src:'overlay/18.png',loaded:false},{src:'overlay/19.png',loaded:false},{src:'overlay/20.png',loaded:false},{src:'overlay/21.png',loaded:false},{src:'overlay/22.png',loaded:false},{src:'overlay/23.png',loaded:false},{src:'overlay/24.png',loaded:false},];var overlayIDs=new Map();var index=0;$(overlays).each(function(){overlayIDs[this.src]=index;index++;});var overlayChangeEnabled=false;var overlaysLoaded=0;var nextOverlay=0;var colors=['#000000','#663300','#ff6666','#ff66ff','#ffcccc','#d3d3d3',];var nextColor=0;var interval=0;function showNewsletter(){interval=setInterval(function(){$('#newsletter').show();$('#mc_embed_signup').show();clearInterval(interval);},1000);}
+function hideNewsletter(){$('#newsletter').hide();$('#mc_embed_signup').hide();}
+function initOverlays(){}
+function preload(){for(var i=0;i<overlays.length;i++){var img=new Image();img.onload=function(event){var key=event.target.attributes['src'].nodeValue;overlays[overlayIDs[key]].loaded=true;overlaysLoaded++;}
+img.src=overlays[i].src;}}
+function changeOverlay(){if(overlaysLoaded==overlays.length){$('#overlay').css('background-image','url('+overlays[nextOverlay].src+')')
+if(nextOverlay==overlays.length-1)
+nextOverlay=0;else
+nextOverlay++;}}
+function changeBackgroundColor(forward){$('.bg-color').animate({backgroundColor:colors[nextColor]},7000);if(nextColor==colors.length-1)
+nextColor=0;else
+nextColor++;}
+$(document).scroll(function(event){var topAfter=$(document).scrollTop();$('#overlay').css('margin-top',topAfter);if(scrollCounter>scrollThreshold){changeOverlay();scrollCounter=0;}
+scrollCounter++;});$(document).ready(function(){preload();});$(window).resize(function(){});$('#mc_embed_signup .email').click(function(){});$('#mc_embed_signup .email').focusin(function(){$('#mc_embed_signup .button').show();$('#mc_embed_signup .email').attr("placeholder","");});$('#mc_embed_signup .button').click(function(){$('#newsletter-notice').hide();});$('#mc_embed_signup .email').focusout(function(){if($('#mc_embed_signup .email').val()===""){$('#mc_embed_signup .email').attr("placeholder","Get 1 notification for offline events");$('div[for="mce-EMAIL"]').hide();$('#mc_embed_signup .button').hide();$('#newsletter-notice').show();}});;(function(){'use strict';if(!document.queryCommandSupported('copy')){return;}
+function flashCopyMessage(el,msg){el.textContent=msg;setTimeout(function(){el.textContent="Copy";},1000);}
+function selectText(node){var selection=window.getSelection();var range=document.createRange();range.selectNodeContents(node);selection.removeAllRanges();selection.addRange(range);return selection;}
+function addCopyButton(containerEl){var copyBtn=document.createElement("button");copyBtn.className="highlight-copy-btn";copyBtn.textContent="Copy";var codeEl=containerEl.firstElementChild;copyBtn.addEventListener('click',function(){try{var selection=selectText(codeEl);document.execCommand('copy');selection.removeAllRanges();flashCopyMessage(copyBtn,'Copied!')}catch(e){console&&console.log(e);flashCopyMessage(copyBtn,'Failed :\'(')}});containerEl.appendChild(copyBtn);}
+var highlightBlocks=document.getElementsByClassName('highlight');Array.prototype.forEach.call(highlightBlocks,addCopyButton);})();
